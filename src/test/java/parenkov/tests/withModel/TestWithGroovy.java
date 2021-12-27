df96624d2a2797c8bcb8607082b6371adeee2c35
ParenkovLocal <parenkov.fedor@gmail.com>
2021-12-27 20:41:24 +0400

    Edited test with groovy

--- src/test/java/parenkov/tests/withModel/TestWithGroovy.java
+++ src/test/java/parenkov/tests/withModel/TestWithGroovy.java
@@ -15,9 +15,9 @@ import static parenkov.tests.withModel.Specs.baseSuccessfulResponse;
 public class TestWithGroovy {
 
     @Test
-    @DisplayName("New user creation")
-    void createUser() {
-        step("Create new user", () -> {
+    @DisplayName("Finding a user by name in the list of users")
+    void findUserByName() {
+        step("Find a user by name", () -> {
             given()
                     .filter(customLogFilter().withCustomTemplates())
                     .spec(baseRequest)
@@ -27,9 +27,27 @@ public class TestWithGroovy {
                     .spec(baseSuccessfulResponse)
                     .log().status()
                     .log().body()
+
                     // выборка имен юзеров, начинающихся на букву "E" и проверка наличия конкретного имени
                     .body("data.findAll{it.first_name =~/^E/}.first_name.flatten()",
-                            hasItem("Emma"))
+                            hasItem("Emma"));
+        });
+    }
+
+    @Test
+    @DisplayName("Finding a user by surname in the list of users")
+    void findUserBySurname() {
+        step("Find a user by surname", () -> {
+            given()
+                    .filter(customLogFilter().withCustomTemplates())
+                    .spec(baseRequest)
+                    .when()
+                    .get("/users?page=1")
+                    .then()
+                    .spec(baseSuccessfulResponse)
+                    .log().status()
+                    .log().body()
+
                     // выборка всех фамилий юзеров и проверка наличия конкретной фамилии
                     .body("data.findAll{it.last_name =~/./}.last_name.flatten()",
                             hasItem("Morris"));
